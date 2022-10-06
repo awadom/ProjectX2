f8a026b6efefd43ab2cde5c79346f73751f4dac9
denfushi <awadomar@gmail.com>
2022-10-06 14:01:55 -0700

    added sign in screen component and index and then displayed it on App.js

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {SafeAreaView, StyleSheet, Text} from 'react-native';
+import SignInScreen from './src/screens/SignInScreen/SignInScreen';
 
 const App = () => {
   return (
     <SafeAreaView style={styles.root}>
-      <Text>Hello World</Text>
+      <SignInScreen />
     </SafeAreaView>
   );
 };
