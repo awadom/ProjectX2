606819d3671ed8cb79243c62bb1a478532534a21
denfushi <awadomar@gmail.com>
2022-10-06 18:39:15 -0700

    added logo to app

--- src/screens/SignInScreen/SignInScreen.js
+++ src/screens/SignInScreen/SignInScreen.js
@@ -1,12 +1,34 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from 'react';
+import {View, Text, Image, StyleSheet, useWindowDimensions} from 'react-native';
+import Logo from '../../../assets/images/ProjectX-Logo.png';
+import CustomInput from '../../components/CustomInput';
 
 const SignInScreen = () => {
+  const {height} = useWindowDimensions();
+
   return (
-    <View>
-      <Text>Sign In Screen</Text>
+    <View style={styles.root}>
+      <Image 
+        source={Logo} 
+        style={[styles.logo, {height: height * 0.3}]} 
+        resizeMode="contain" 
+      />
+
+      <CustomInput />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  root:{
+    alignItems: 'center',
+    padding: 20,
+  },
+  logo: {
+    width: '100%',
+    maxWidth: 500,
+    maxHeight: 300,
+  },
+});
 
 export default SignInScreen
