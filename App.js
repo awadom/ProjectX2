1dd2aa62ce6ca3c915593f3baecaa7cf1401899b
denfushi <awadomar@gmail.com>
2022-10-10 13:56:41 -0700

    beginning signUpScreen and uploading image preview for SignInScreen

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {SafeAreaView, StyleSheet, Text} from 'react-native';
 import SignInScreen from './src/screens/SignInScreen/SignInScreen';
+import SignUpScreen from './src/screens/SignUpScreen/SignUpScreen';
 
 const App = () => {
   return (
---
+++ src/screens/SignUpScreen/SignUpScreen.js
@@ -0,0 +1,108 @@
+import React, {useState} from 'react'; 
+import {View, Text, Image, StyleSheet, useWindowDimensions, ScrollView} from 'react-native'; 
+import Logo from '../../../assets/images/ProjectX-Logo.png';
+import CustomInput from '../../components/CustomInput';
+import CustomButton from '../../components/CustomButton';
+import { Scope } from '@babel/traverse';
+
+const SignUpScreen = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const {height} = useWindowDimensions();
+
+  const onSignInPressed = () => {
+    console.warn("Sign in");
+  }
+
+  const onForgotPasswordPressed = () => {
+    console.warn("Forgot Password?");
+  }
+
+  const onSignInGoogle = () => {
+    console.warn("Google");
+  }
+
+  const onSignInFacebook = () => {
+    console.warn("Facebook");
+  }
+
+  const onSignInApple = () => {
+    console.warn("Apple");
+  }
+
+  const onSignUpPressed = () => {
+    console.warn("SignUp");
+  }
+  return (
+    <ScrollView showsVerticalScrollIndicator={false}>
+      <View style={styles.root}>
+        <Text style={styles.title}>Create an account</Text>
+        
+
+        <CustomInput 
+          placeholder = "Username" 
+          value={username} 
+          setValue={setUsername} 
+        />
+        <CustomInput 
+          placeholder="Password" 
+          value={password} 
+          setValue={setPassword} 
+          secureTextEntry
+        />
+        <CustomButton text = "Sign In" onPress={onSignInPressed}/>
+        <CustomButton 
+          text = "Forgot Password?" 
+          onPress={onForgotPasswordPressed} 
+          type="TERTIARY" 
+        />
+
+        <CustomButton 
+          text = "Sign In with Google" 
+          onPress={onSignInGoogle}
+          bgColor ="#FAE9EA"
+          fgColor ="#DD4D44"
+        />
+        <CustomButton 
+          text = "Sign In with Facebook" 
+          onPress={onSignInFacebook}
+          bgColor ="#E7EAF4"
+          fgColor ="#4765A9"
+        />
+        <CustomButton 
+          text = "Sign In with Apple" 
+          onPress={onSignInApple}
+          bgColor ="#e3e3e3"
+          fgColor ="#363636"
+        />
+
+        <CustomButton 
+          text = "Don't have an account? Create one" 
+          onPress={onSignUpPressed} 
+          type="TERTIARY" 
+        />
+      </View>
+    </ScrollView>
+  )
+}
+
+const styles = StyleSheet.create({
+  root:{
+    alignItems: 'center',
+    padding: 20,
+  },
+  logo: {
+    width: '100%',
+    maxWidth: 500,
+    maxHeight: 300,
+  },
+  title:{
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#051C60',
+    margin: 10,
+  }
+});
+
+export default SignUpScreen
