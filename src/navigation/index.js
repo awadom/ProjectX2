61fa3783683f7d356f88b680771fc513dadfd65f
denfushi <awadomar@gmail.com>
2022-10-10 18:56:45 -0700

    fixed bugs, DefaultTheme added to react-navigation for background color

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,6 +1,6 @@
 import {View, Text} from 'react-native';
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
+import {NavigationContainer, DefaultTheme} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import SignInScreen from '../screens/SignInScreen';
 import SignUpScreen from '../screens/SignUpScreen';
@@ -11,9 +11,17 @@ import HomeScreen from '../screens/HomeScreen';
 
 const Stack = createNativeStackNavigator();
 
+const MyTheme = {
+  DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: '#0582FF',
+  },
+};
+
 const Navigation = () => {
   return (
-    <NavigationContainer>
+    <NavigationContainer theme={MyTheme}>
       <Stack.Navigator screenOptions={{headerShown: false}}>
         <Stack.Screen name="SignIn" component={SignInScreen} />
         <Stack.Screen name="SignUp" component={SignUpScreen} />
