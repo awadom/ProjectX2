c02e91d5bab18a36ca5ae52d7e81db42295dc064
denfushi <awadomar@gmail.com>
2022-10-10 21:53:14 -0700

    registered email user with no errors

--- firebase/firebase-config.js
+++ firebase/firebase-config.js
@@ -1,5 +1,4 @@
 import {initializeApp} from 'firebase/app';
-import {getAnalytics} from 'firebase/analytics';
 import {getAuth} from 'firebase/auth';
 
 const firebaseConfig = {
@@ -15,6 +14,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 export const authentication = getAuth(app);
